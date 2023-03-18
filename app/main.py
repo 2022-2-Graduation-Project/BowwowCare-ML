75daffd0fe167503fc44b60255942df9b84b434e
hee-suh <suhee0206@gmail.com>
2023-03-18 15:56:47 +0900

    [#] feat: change model - from resnet18 to efficientnet-b0 - from 4 classes to 3 classes(positive, angry, sad)

--- app/main.py
+++ app/main.py
@@ -10,8 +10,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 
 import torch
-import torchvision.models as models
 from torchvision import transforms
+
+import timm
 
 from config import CONFIG
 
@@ -24,11 +25,10 @@ app = FastAPI(
     contact=None,
     license_info=None
 )
-# python main.py or uvicorn main:app --workers 2 —-port 8080 —-log-config log.ini -—reload
+# python main.py OR uvicorn main:app --workers 2 —-port 8000 —-log-config log.ini -—reload
 
 # Allow CORS for local debugging
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
-
 
 @app.get("/")
 def read_root():
@@ -44,7 +44,7 @@ async def startup_event():
     logger.info('PyTorch using device: {}'.format(CONFIG['DEVICE']))
 
     # Initialize the pytorch model
-    model = models.resnet18()
+    model = timm.create_model('efficientnet_b0', num_classes=3, pretrained=True)
     model.load_state_dict(torch.load(
         CONFIG['MODEL_PATH'], map_location=torch.device(CONFIG['DEVICE'])))
     model.eval()
@@ -53,7 +53,6 @@ async def startup_event():
     app.package = {
         "model": model
     }
-
 
 @app.post('/api/v1/predict')
 async def do_predict(file: UploadFile=File(...)):
@@ -68,7 +67,6 @@ async def do_predict(file: UploadFile=File(...)):
     image = Image.open(io.BytesIO(request_object_content))
 
     X = transforms.ToTensor()(image).unsqueeze_(0)
-    # X = image
 
     model = app.package['model']
     with torch.no_grad():
@@ -85,7 +83,7 @@ async def do_predict(file: UploadFile=File(...)):
     y = y_pred[0]
 
     # generate prediction based on probablity
-    pred = ['angry', 'happy', 'relaxed', 'sad'][y.argmax()]
+    pred = ['angry', 'positive', 'sad'][y.argmax()]
 
     # round probablities for json
     y = y.tolist()
@@ -94,9 +92,8 @@ async def do_predict(file: UploadFile=File(...)):
     # prepare json for returning
     results = {
         'angry': y[0],
-        'happy': y[1],
-        'relaxed': y[2],
-        'sad': y[3],
+        'positive': y[1],
+        'sad': y[2],
         'pred': pred
     }
 
@@ -105,7 +102,6 @@ async def do_predict(file: UploadFile=File(...)):
     return {
         "emotion": pred.upper()
     }
-
 
 @app.get('/about')
 def show_about():
@@ -130,6 +126,6 @@ def show_about():
 
 if __name__ == '__main__':
     # server api
-    uvicorn.run("main:app", host="0.0.0.0", port=8080,
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
                 reload=True, log_config="log.ini"
                 )
